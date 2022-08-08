4802e7e3142c15bf8ae89f4014cb2654222dfa9c
samisamsoom <93786812+samisamsoom@users.noreply.github.com>
2022-08-08 23:57:31 +0300

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,9 @@
+ubjects_with_no_students = {"Arabic":3,  "English":3,"math":2}
+print(ubjects_with_no_students)
+students = {"Arabic":["Ali", "Mohammed","Mona"], "English":["Jack", "Ahmed","Munif"]}
+print(students)
+students['Math'] = ['Salah','Hussein']
+print(students)
+students.update({"History":'Sami'})
+print(students)
+
