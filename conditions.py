8a5dc56d8b38b92bb89645df9b393ffc55342d0c
samisamsoom <93786812+samisamsoom@users.noreply.github.com>
2022-08-08 22:18:43 +0300

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,9 @@
+from re import A
+
+
+username = input('Please insert your userName: ')
+password = input('Please insert your password: ')
+if username == 'sami' and password == '123':
+    print ('Welocme to your email')
+else: 
+    print('Error')
